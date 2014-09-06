d966a657fd6dfb7ce7e570d41ba2d224cf74ee3d
Ilansennin <[jl1616_16@outlook.es]>
2014-09-06 09:57:55 -0500

    Prueba de mensaje

---
+++ script.js
@@ -0,0 +1,9 @@
+function valor(){
+document.getElementById("numero").value="120";
+document.getElementById("demo").innerHTML = Date();
+}
+
+function cambiarColor(){
+document.getElementById("numero2").color=red;
+document.getElementById("numero2").value="jajajajaja";
+}
